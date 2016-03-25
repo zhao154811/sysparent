29de22a0d51fd73c5646805696f70c8b75c886aa
154811696 <z154811696@gmail.com>
2016-03-25 15:48:36 +0800

    Update SexEnumHandler.java

--- syscommon/src/main/java/com/wenyu/typehandler/SexEnumHandler.java
+++ syscommon/src/main/java/com/wenyu/typehandler/SexEnumHandler.java
@@ -11,7 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Created by zhaowenyu@ucredit.com on 2015/11/10.
  */
 public class SexEnumHandler extends BaseTypeHandler<SexEnum> {
     private Class<SexEnum> type;
