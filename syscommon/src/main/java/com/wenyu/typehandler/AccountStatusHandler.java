7f0eb8ac5db8f43b060ecca9d81edb2963e7a1a8
154811696 <z154811696@gmail.com>
2016-03-25 15:43:24 +0800

    Update AccountStatusHandler.java

--- syscommon/src/main/java/com/wenyu/typehandler/AccountStatusHandler.java
+++ syscommon/src/main/java/com/wenyu/typehandler/AccountStatusHandler.java
@@ -11,7 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Created by zhaowenyu@ucredit.com on 2015/11/10.
  */
 public class AccountStatusHandler extends BaseTypeHandler<AccountStatus> {
     private Class<AccountStatus> type;
