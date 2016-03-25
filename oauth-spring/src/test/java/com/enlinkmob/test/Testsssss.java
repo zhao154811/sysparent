30a0c2cf47b8f4e824f9613a80133cd64425310a
154811696 <z154811696@gmail.com>
2016-03-25 15:04:25 +0800

    Update Testsssss.java

--- oauth-spring/src/test/java/com/enlinkmob/test/Testsssss.java
+++ oauth-spring/src/test/java/com/enlinkmob/test/Testsssss.java
@@ -4,7 +4,6 @@ package com.enlinkmob.test;
 import com.wenyu.Enum.SexEnum;
 
 /**
- * Created by zhaowenyu@ucredit.com on 2015/12/4.
  */
 public class Testsssss {
     public static void main(String args[]) {
